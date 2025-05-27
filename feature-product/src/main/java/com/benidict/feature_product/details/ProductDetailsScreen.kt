a32b60eec40a4f908141ef82b81bc43c3a5de54a
Benidict Dulce <benidict.programmer@gmail.com>
2025-05-27 23:33:19 +0800

    feat: product details screen

--- feature-product/src/main/java/com/benidict/feature_product/details/ProductDetailsScreen.kt
+++ feature-product/src/main/java/com/benidict/feature_product/details/ProductDetailsScreen.kt
@@ -9,11 +9,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -44,12 +50,15 @@ fun ProductDetailsScreen(navController: NavHostController, productId: Int) {
 
         Column(
             modifier = Modifier
-                .padding(paddingValues)
-                .padding(horizontal = 16.dp)
+                .verticalScroll(rememberScrollState())
                 .fillMaxSize()
-
         ) {
-            Row(modifier = Modifier.fillMaxWidth()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(paddingValues)
+                    .padding(horizontal = 16.dp)
+            ) {
                 CircleBackButton {
                     navController.popBackStack()
                 }
@@ -57,28 +66,63 @@ fun ProductDetailsScreen(navController: NavHostController, productId: Int) {
             ImageLoader(
                 url = product.value.productImageUrl,
                 modifier = Modifier
+                    .background(color = Color.White)
                     .fillMaxWidth()
                     .height(400.dp)
             )
-            Text(
-                text = product.value.productName,
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-            )
-            Text(
-                text = product.value.weight,
-                fontSize = 15.sp,
-                fontWeight = FontWeight.Normal,
-                color = Color.Gray
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = product.value.price.convertToPeso(),
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.Gray
-            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Color.White)
+                    .clip(RoundedCornerShape(16.dp))
+            ) {
+                Text(
+                    text = product.value.productName,
+                    fontSize = 26.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .padding(top = 10.dp)
+                )
+                Text(
+                    text = product.value.weight,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = Color.Gray,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = product.value.price.convertToPeso(),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Gray,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .padding(bottom = 10.dp)
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+            }
 
+            Spacer(modifier = Modifier.height(4.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Color.White)
+                    .clip(RoundedCornerShape(16.dp))
+            ) {
+                Text(
+                    text = "Description", fontSize = 20.sp, fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier.padding(16.dp, top = 10.dp)
+                )
+                Text(
+                    text = product.value.productDescription,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal,
+                    modifier = Modifier.padding(16.dp, vertical = 10.dp)
+                )
+            }
         }
     }
 }
